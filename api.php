cb44185ac44093278be42dfa059cf42f69313e5e
Starbeamrainbowlabs <starbeamrainbowlabs@gmail.com>
2015-01-05 14:04:32 +0000

    Added checklogin action

--- api.php
+++ api.php
@@ -188,8 +188,28 @@ switch($_GET["action"])
 		
 		http_response_code(200);
 		$response = new api_response(200, 0, "Login successful.");
+		$response->user = $_POST["user"];
 		$response->sessionkey = $sessionkey;
 		$response->expire_time = time() + $session_key_valid_time;
+		sendjson($response);
+		exit();
+		break;
+	
+	/*
+	 *       _               _    _             _       
+	 *   ___| |__   ___  ___| | _| | ___   __ _(_)_ __  
+	 *  / __| '_ \ / _ \/ __| |/ / |/ _ \ / _` | | '_ \ 
+	 * | (__| | | |  __/ (__|   <| | (_) | (_| | | | | |
+	 *  \___|_| |_|\___|\___|_|\_\_|\___/ \__, |_|_| |_|
+	 *                                    |___/         
+	 */
+	case "checklogin":
+		$response = new api_response(200, 0, "checklogin");
+		$response->logged_in = $logged_in;
+		if(isset($user))
+			$response->user = $user;
+		else
+			$response->user = false;
 		sendjson($response);
 		exit();
 		break;
