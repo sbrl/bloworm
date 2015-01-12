198732392d4ee72dccb9eb3de87844f9989389cb
Starbeamrainbowlabs <starbeamrainbowlabs@gmail.com>
2015-01-12 15:12:35 +0000

    reworked bookmark renderer, still untested

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -27,54 +27,29 @@ blow_worm.actions.bookmarks = {
 	
 	//function to convert a bookmark object to the appropriate html
 	render: function(bookmark) {
-		/*
-		 * <div class='bookmark' data-id='...'>
-		 *	<input type='checkbox' class='bookmark-favicon' />
-		 *	<div class='bookmark-details flex down'>
-		 *		<div class='top-row'>
-		 * 			<span class='bookmark-name'></span>
-		 * 			<span class='bookmark-date'></span>
-		 * 		</div>
-		 * 		<div class='bottom-row'>
-		 * 			<span class='bookmark-url'></span>
-		 * 			<span class='bookmark-tags'>
-		 * 				<span class='tag'></span>
-		 * 				<span class='tag'></span>
-		 * 				<span class='tag'></span>
-		 * 			</span>
-		 * 		</div>
-		 * 	</div>
-		 * </div>
-		 */
 		var html = document.createElement("div");
-		html.classList.add("bookmark", "flex", "across");
 		html.dataset.id = bookmark.id;
+		html.innerHTML = "<div class='bookmark' data-id='...'>" +
+		"<input type='checkbox' class='bookmark-favicon' />" + 
+		"<div class='bookmark-details flex down'>" + 
+		"	<div class='top-row'>" + 
+		"			<span class='bookmark-name'></span>" + 
+		"			<span class='bookmark-date'></span>" + 
+		"		</div>" + 
+		"		<div class='bottom-row'>" + 
+		"			<span class='bookmark-url'></span>" + 
+		"			<span class='bookmark-tags'></span>" + 
+		"		</div>" + 
+		"	</div>" + 
+		"</div>";
 		
-		html.appendChild(document.createElement("input"))
-		html.querySelector("input").type = "checkbox";
-		html.querySelector("input").dataset["favicon-url"] = bookmark.faviconurl;
-		html.querySelector("input").classList.add("bookmark-favicon");
+		//insert the name / url
+		html.querySelector(".bookmark-name").innerText = bookmark.name;
+		html.querySelector(".bookmark-url").innerText = bookmark.url;
 		
-		html.appendChild(document.createElement("div"));
-		html.querySelector("div").classList.add("bookmark-details", "flex", "down");
-		html.querySelector(".bookmark-details").appendChild(document.createElement("div"))
-		html.querySelector(".bookmark-details div").classList.add("top-row");
-		html.querySelector(".bookmark-details").appendChild(document.createElement("div"))
-		html.querySelectorAll(".bookmark-details div:nth-child(2)").classList.add("bottom-row");
+		//todo format and add the date
 		
-		html.querySelector(".top-row").appendChild(document.createElement("span"));
-		html.querySelector(".top-row").appendChild(document.createElement("span"));
-		html.querySelector(".top-row span:nth-child(1)").classList.add("bookmark-name");
-		html.querySelector(".top-row span:nth-child(2)").classList.add("bookmark-date");
-		
-		html.querySelector(".bottom-row").appendChild(document.createElement("span"));
-		html.querySelector(".bottom-row").appendChild(document.createElement("span"));
-		html.querySelector(".bottom-row span:nth-child(1)").classList.add("bookmark-url");
-		html.querySelector(".bottom-row span:nth-child(2)").classList.add("bookmark-tags");
-		
-		html.querySelector(".bookmark-name").appendChild(document.createTextNode(bookmark.name));
-		html.querySelector(".bookmark-url").appendChild(document.createTextNode(bookmark.url));
-		
+		//insert the tags
 		var html_tags = html.querySelector(".bookmark-tags");
 		bookmark.tags.forEach(function(tag) {
 			var new_tag_html = document.createElement("span");
@@ -82,8 +57,6 @@ blow_worm.actions.bookmarks = {
 			new_tag_html.appendChild(document.createTextNode(tag));
 			html_tags.appendChild(new_tag_html);
 		});
-		
-		//todo format and add the date
 		
 		return html;
 	},
