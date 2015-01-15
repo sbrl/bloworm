bacd7f1b0eee9eeb747629a7eacf37bf050d9b59
Starbeamrainbowlabs <starbeamrainbowlabs@gmail.com>
2015-01-15 09:50:26 +0000

    added param validation to adduser admin action

--- actions/reqadm/adduser.php
+++ actions/reqadm/adduser.php
@@ -8,6 +8,15 @@
  *                               %adduser%
  */
 
+if(!isset($_GET["newusername"]))
+	senderror(new api_response(449, 581, "No username was specified for the new account."));
+
+if(preg_match("/[^a-z0-9\-_]/i", $_GET["newusername"]))
+	senderror(new api_response(400, 582, "User account creation failed: Potentially dangerous characters were found in the new username."));
+
+if(file_exists(get_user_data_dir_name($_GET["newusername"])))
+	senderror(507, 151, "That user account already exists.");
+
 http_response_code(501);
 exit("This action is not implemented yet.");
 
