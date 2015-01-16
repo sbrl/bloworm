772061ab0d599c9efaf2c974fd4e402d57c864df
Starbeamrainbowlabs <starbeamrainbowlabs@gmail.com>
2015-01-16 12:34:23 +0000

    added (untested) code to delete bookmarks

--- actions/reql/delete.php
+++ actions/reql/delete.php
@@ -13,7 +13,31 @@ if(isset($_GET["id"]))
 else
 	senderror(new api_error(449, 504, "You didn't specify an `id` to delete.\n\nThe appropriate GET parameter is `id`."));
 
-http_response_code(501);
-exit("This action is not implemented yet.");
+$bookmarks = getjson(user_dirname($user) . "bookmarks.json");
+for($i = count($bookmarks); $i >= 0; $i++)
+{
+	if($bookmarks[$i]->id == $id_to_delete)
+	{
+		$tags = $bookmarks[$i]->tags;
+		unset($bookmarks);
+		
+		$bookmarks = array_values($bookmarks);
+		
+		setjson(user_dirname($user) . "bookmarks.json");
+		
+		$all_tags = getjson(user_dirname($user) . "tags.json");
+		foreach($tags as $tag)
+		{
+			$tags->$tag--;
+			if($tags->$tag == 0)
+				unset($tags->$tag);
+		}
+		setjson(user_dirname($user) . "tags.json");
+		
+		$response = new api_response(200, 0, "Bookmark deletion completed.");
+	}
+}
+
+senderror(new api_error(400, 509, "A bookmark with that id could not be found."));
 
 ?>
