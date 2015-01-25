0fe0d98a507dde088dc6489c66c2e33d1b3df6ef
Starbeamrainbowlabs <starbeamrainbowlabs@gmail.com>
2015-01-25 18:42:22 +0000

    added untested api password changing support

--- actions/reql/usermod.php
+++ actions/reql/usermod.php
@@ -9,16 +9,31 @@
  */
 
 //key, value
-if(isset($_GET["key"]))
-	$key = $_GET["key"];
+if(isset($_POST["key"]))
+	$key = $_POST["key"];
 else
 	senderror(new api_error(449, 507, "No key was specified.\n\nThe appropriate GET parameter is `key`."));
-if(isset($_GET["value"]))
-	$value = $_GET["value"];
+if(isset($_POST["value"]))
+	$value = $_POST["value"];
 else
 	senderror(new api_error(449, 508, "No value was specified.\n\nThe approapriate GET parameter is `value`."));
 
-http_response_code(501);
-exit("This action is not implemented yet.");
+switch($key)
+{
+	case "password":
+		if(!isset($_POST["oldpass"]))
+			senderror(new api_error(449, 509, "You did't specify your old password, so blow worm can't change it to your new one."));
+		
+		if(!password_verify($_POST["oldpass"], file_get_contents(user_dirname($user) . "password")))
+			senderror(new api_error(401, 130, "You didn't type your old password correctly."));
+		
+		file_put_contents(user_dirname($user) . "password", hash_password($_POST["value"]));
+		
+		$response = new api_response(200, 0, "usermod/passwordchange/success");
+		sendjson($response);
+	
+	default:
+		senderror(new api_error(400, 510, "That user setting key was not found."));
+}
 
 ?>
