6a02262c0c0818d95c32746e19725d6223c85dc3
Starbeamrainbowlabs <starbeamrainbowlabs@gmail.com>
2015-01-26 11:36:20 +0000

    implemented (untested) adduser action in api

--- actions/reqadm/adduser.php
+++ actions/reqadm/adduser.php
@@ -13,11 +13,32 @@ if(!isset($_GET["newusername"]))
 
 if(preg_match("/[^a-z0-9\-_]/i", $_GET["newusername"]))
 	senderror(new api_response(400, 582, "User account creation failed: Potentially dangerous characters were found in the new username."));
+else
+	$newusername = $_GET["newusername"];
 
 if(file_exists(user_dirname($_GET["newusername"])))
 	senderror(507, 151, "That user account already exists.");
 
-http_response_code(501);
-exit("This action is not implemented yet.");
+$password = base_convert(uniqid("", true), 10, 36);
+
+// create a the new user's directory tree
+create_tree([
+	[ "type" => "folder", "mode" => 0700, "path" => "users/$newusername" ],
+	[ "type" => "file", "mode" => 0700 , "path" => "users/$newusername/password", "content" => hash_password($password)],
+	[ "type" => "file", "mode" => 0700, "path" => "users/$newusername/bookmarks.json", "content" => "[]" ],
+	[ "type" => "file", "mode" => 0700, "path" => "users/$newusername/isadmin", "content" => "false" ],
+	[ "type" => "file", "mode" => 0700, "path" => "users/$newusername/tags.json", "content" => "{}" ]
+]);
+
+// add the new username to the user list
+$userlist = getjson("data/userlist.json");
+$userlist[] = $newusername;
+setjson("data/userlist.json", $userlist);
+
+$response = new api_response(201, 0, "New user created successfully.");
+$response->username = $newusername;
+$response->password = $password;
+sendjson($response);
+exit();
 
 ?>
