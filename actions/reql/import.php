4a8b238177f04e98ae1267d90dc37c36e60521dc
Starbeamrainbowlabs <starbeamrainbowlabs@gmail.com>
2015-01-30 09:51:58 +0000

    started import action, haven't done non-overwrite imports yet and is untested

---
+++ actions/reql/import.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ *  _                            _   
+ * (_)_ __ ___  _ __   ___  _ __| |_ 
+ * | | '_ ` _ \| '_ \ / _ \| '__| __|
+ * | | | | | | | |_) | (_) | |  | |_ 
+ * |_|_| |_| |_| .__/ \___/|_|   \__|
+ *             |_|                   
+ */
+
+if(isset($_GET["overwrite"]) and ($_GET["overwrite"] == "true" or $_GET["overwrite"] == "yes"))
+	$overwrite = true;
+else
+	$overwrite = false;
+
+$request_headers = getallheaders();
+$request_headers = array_change_key_case($request_headers);
+header("content-type: application/json");
+var_dump($request_headers);
+
+if(!isset($request_headers["content-length"]))
+	senderror(new api_error(400, 530, "No content-length header was present in the request."));
+
+if($request_headers["content-length"] > 10e6)
+	senderror(new api_error(507, 531, "You tried to send too much data to the server. Please contact the administrator of this bloworm installation to tget your data imported manually."));
+
+$data = file_get_contents("php://input");
+$data = gzdecode($data);
+if(!$data)
+	senderror(new api_error(400, 532, "An error occurred whilst trying to decompress the data you sent."));
+
+try {
+	$to_import = json_decode($data);
+} catch(Exception $error) {
+	senderror(new api_error(400, 533, "An error occurred while parsing the data you sent."));
+}
+
+// todo unify this so that the list of files that a user has are in one master array
+// note a similar array is defined in the export action
+$filenames = [
+	"bookmarks.json",
+	"tags.json"
+];
+foreach($to_import as $key => $value)
+{
+	if(!in_array($key, $filenames))
+		continue; // this filename isn't part of the user's data, leave it alone
+	
+	if($overwrite === true)
+		setjson(user_dirname($user) . $key, $value);
+	else
+	{
+		// todo merge the existing file with the new one
+		http_response_code(501);
+		exit("Non overwriting imports are not implemented yet.");
+	}
+};
+
+$response = new api_response(201, 0, "import/" . ($overwrite ? "overwrite" : "non-overwrite") . "/success");
+sendjson($response);
+
+?>
